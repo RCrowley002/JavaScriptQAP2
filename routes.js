78d1b2c263599236149061f30b1861d0056f6507
Ryan Crowley <ryan.g.crowley@gmail.com>
2023-10-13 15:28:49 -0230

    starting task 2

---
+++ routes.js
@@ -0,0 +1,25 @@
+// creating functions to route my files
+const fs = require("fs");
+
+function aboutPage(path, response) {
+  displayFile(path, response);
+}
+
+function blogPage(path, response) {
+  displayFile(path, response);
+}
+function contactPage(path, response) {
+  displayFile(path, response);
+}
+function developementPage(path, response) {
+  displayFile(path, response);
+}
+function indexPage(path, response) {
+  displayFile(path, response);
+}
+function productsPage(path, response) {
+  displayFile(path, response);
+}
+function subscribePage(path, response) {
+  displayFile(path, response);
+}
